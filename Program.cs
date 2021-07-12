9001031bd89f82f3b9bbd4125e594b791366fcbc
GameBoyYeet <ericshurocks@gmail.com>
2021-07-12 15:06:08 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BrowserSpam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type the URL of the website you would like to spam and press Enter. You can right-click on the taskbar icon to exit. ");
+            Console.Out.Flush();
+            string page = Console.ReadLine();
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+        }
+    }
+}
